0c6e9a8d70110d2bb79cde921a8f5530ae5279b1
maricindra <marinacindra13@gmail.com>
2026-02-23 14:29:37 -0300

    src update from video background

--- src/components/Home_elements/Video_Background.jsx
+++ src/components/Home_elements/Video_Background.jsx
@@ -9,7 +9,7 @@ function Video_Page() {
           playsInline
           //  Use inset-0 to define the whole space without to show the black borders in the className of video.
           className="absolute inset-0 z-0 w-full h-full object-cover">
-          <source src="/lordring_1.mp4" type="video/mp4" />
+          <source src="/lord_fans/lordring_1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
