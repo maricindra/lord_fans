1205218fb3f12fe42ffc5cf97616f9089833ad98
maricindra <marinacindra13@gmail.com>
2026-02-23 22:54:15 -0300

    footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 
       <div className='flex flex-col items-center justify-center '>
         <h1 className="text-golden_old center font-cocogothic tracking-[0.4em] uppercase">Lord of the ring</h1>
-        <Spline className="w-8 h-80 md:w-100 md:h-100 justify-center items-center flex flex-col mx-auto py-10 gap-10"
+        <Spline className="w-40 h-40 md:w-100 md:h-100 justify-center items-center flex flex-col mx-auto py-10 gap-10"
         scene="https://prod.spline.design/dEMJaIsLBCWJx-2w/scene.splinecode" />
       
     <Playlist/>
